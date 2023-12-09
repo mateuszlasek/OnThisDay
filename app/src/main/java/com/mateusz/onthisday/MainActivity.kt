3547039e4ba79be1eecad68361012a250b53c528
serwus997 <mateusz.lasek17@wp.pl>
2023-12-09 23:49:33 +0100

    updated bottom nav

--- app/src/main/java/com/mateusz/onthisday/MainActivity.kt
+++ app/src/main/java/com/mateusz/onthisday/MainActivity.kt
@@ -13,13 +13,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -44,13 +47,13 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 
-data class NavigationItem(
+data class BottomNavigationItem(
     val title: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
     val badgeCount: Int? = null
 )
-
 @OptIn(ExperimentalMaterial3Api::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -63,111 +66,99 @@ class MainActivity : ComponentActivity() {
             OnThisDayTheme {
                 val navController = rememberNavController()
                 val items = listOf(
-                    NavigationItem(
-                        title = "All",
+                    BottomNavigationItem(
+                        title = "Home",
                         selectedIcon = Icons.Filled.Home,
                         unselectedIcon = Icons.Outlined.Home,
+                        hasNews = false,
                     ),
-                    NavigationItem(
-                        title = "Urgent",
-                        selectedIcon = Icons.Filled.Info,
-                        unselectedIcon = Icons.Outlined.Info,
-                        badgeCount = 45
+                    BottomNavigationItem(
+                        title = "Favourites",
+                        selectedIcon = Icons.Filled.Star,
+                        unselectedIcon = Icons.Outlined.Star,
+                        hasNews = false,
                     ),
-                    NavigationItem(
+                    BottomNavigationItem(
                         title = "Settings",
                         selectedIcon = Icons.Filled.Settings,
                         unselectedIcon = Icons.Outlined.Settings,
+                        hasNews = true,
                     ),
                 )
-
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                    val scope = rememberCoroutineScope()
-                    var selectedItemIndex by rememberSaveable {
-                        mutableStateOf(0)
-                    }
-                    ModalNavigationDrawer(
-                        drawerContent = {
-                            ModalDrawerSheet {
-                                Spacer(modifier = Modifier.height(16.dp))
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar {
                                 items.forEachIndexed { index, item ->
-                                    NavigationDrawerItem(
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            navController.navigate(item.title)
+                                        },
                                         label = {
                                             Text(text = item.title)
                                         },
-                                        selected = index == selectedItemIndex,
-                                        onClick = {
-                                            navController.navigate(item.title)
-                                            selectedItemIndex = index
-                                            scope.launch {
-                                                drawerState.close()
-                                            }
-                                        },
+                                        alwaysShowLabel = false,
                                         icon = {
-                                            Icon(
-                                                imageVector = if (index == selectedItemIndex) {
-                                                    item.selectedIcon
-                                                } else item.unselectedIcon,
-                                                contentDescription = item.title
-                                            )
-                                        },
-                                        badge = {
-                                            item.badgeCount?.let {
-                                                Text(text = item.badgeCount.toString())
+                                            BadgedBox(
+                                                badge = {
+                                                    if(item.badgeCount != null) {
+                                                        Badge {
+                                                            Text(text = item.badgeCount.toString())
+                                                        }
+                                                    } else if(item.hasNews) {
+                                                        Badge()
+                                                    }
+                                                }
+                                            ) {
+                                                Icon(
+                                                    imageVector = if (index == selectedItemIndex) {
+                                                        item.selectedIcon
+                                                    } else item.unselectedIcon,
+                                                    contentDescription = item.title
+                                                )
                                             }
-                                        },
-                                        modifier = Modifier
-                                            .padding(NavigationDrawerItemDefaults.ItemPadding)
+                                        }
                                     )
                                 }
                             }
-                        },
-                        drawerState = drawerState
-                    ) {
-                        Scaffold(
-                            topBar = {
-                                TopAppBar(
-                                    title = {
-                                        Text(text = "Todo App")
-                                    },
-                                    navigationIcon = {
-                                        IconButton(onClick = {
-                                            scope.launch {
-                                                drawerState.open()
-                                            }
-                                        }) {
-                                            Icon(
-                                                imageVector = Icons.Default.Menu,
-                                                contentDescription = "Menu"
-                                            )
-                                        }
-                                    }
-                                )
-                            }
-                        ) { innerPadding ->
-                            Column(
-                                modifier = Modifier
-                                    .padding(innerPadding),
-                                verticalArrangement = Arrangement.spacedBy(16.dp),
-                            ) {
-                                val viewModel: EventListViewModel = viewModel()
-                                NavHost(navController = navController, startDestination = "All"){
-                                    composable("All"){
+                        }
+                    ) { paddingValues ->
+                        Column(
+                            modifier = Modifier
+                                .padding(paddingValues),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
 
-                                    }
-                                    composable("Urgent"){
-                                        EventListScreen(viewModel)
-                                     }
+                        ){
+                            val viewModel: EventListViewModel = viewModel()
+                            NavHost(navController = navController, startDestination = "Home"){
+                                composable("Home"){
+                                    EventListScreen(viewModel)
+                                }
+                                composable("Favourites"){
+
+                                }
+                                composable("Settings"){
+
                                 }
                             }
+
                         }
+
+
+
                     }
                 }
-            } //theme end
-        }
+            }
+        } //theme end
     }
 }
+
+
